ce6b132dfc93bc2a88b480c30211bf16f89d3404
darshini418 <priyadarshinireddy5@gmail.com>
2021-12-23 16:44:45 +0530

    added the filename

---
+++ String1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class String1{
+public static void main(String[] args){
+String s1 = "darshini";
+char ch[] = {'d','a','r','s','h','i','n','i'};
+String s2 = new String(ch);
+String s3 = new String("priya");
+System.out.println(s1);
+System.out.println(s2);
+System.out.println(s3);
+}}
